63ed6e8b7bdc21b2020374ed63ab37c5f0542151
PudhiyavanP <102018919+Pudhiyavan0405@users.noreply.github.com>
2023-02-01 15:05:46 +0530

    cloning plugins

---
+++ cordova-plugin-simple-file-chooser/src/android/Chooser.java
@@ -0,0 +1,209 @@
+package in.foobars.cordova;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
+
+import com.itextpdf.text.pdf.PdfReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.Exception;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Chooser extends CordovaPlugin {
+    private static final String ACTION_OPEN = "getFiles";
+    private static final int PICK_FILE_REQUEST = 1;
+    private static final String TAG = "Chooser";
+    public static File rootDirectory = null;
+
+    public static String getDisplayName(ContentResolver contentResolver, Uri uri) {
+        String[] projection = {MediaStore.MediaColumns.DISPLAY_NAME};
+        Cursor metaCursor = contentResolver.query(uri, projection, null, null, null);
+
+        if (metaCursor != null) {
+            try {
+                if (metaCursor.moveToFirst()) {
+                    return metaCursor.getString(0);
+                }
+            } finally {
+                metaCursor.close();
+            }
+        }
+
+        return null;
+    }
+
+    private CallbackContext callback;
+
+    public void chooseFile(CallbackContext callbackContext, String accept) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+          if(accept.equals("image/*")) {
+            intent.setType("image/*");
+        }else if(accept.equals("application/pdf")){
+            intent.setType("application/pdf");
+        }else{
+            intent.setType("image/*|application/pdf");
+        }
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, accept.split(","));
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+      //  intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, false);
+
+        Intent chooser = Intent.createChooser(intent, "Select File");
+        cordova.startActivityForResult(this, chooser, Chooser.PICK_FILE_REQUEST);
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        this.callback = callbackContext;
+        callbackContext.sendPluginResult(pluginResult);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        rootDirectory = new File(cordova.getActivity().getExternalFilesDir(""), "");
+        try {
+            if (action.equals(Chooser.ACTION_OPEN)) {
+                this.chooseFile(callbackContext, args.getString(0));
+                return true;
+            }
+        } catch (JSONException err) {
+            this.callback.error("Execute failed: " + err.toString());
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            if (requestCode == Chooser.PICK_FILE_REQUEST && this.callback != null) {
+                if (resultCode == Activity.RESULT_OK) {
+                    JSONArray files = new JSONArray();
+                    if (data.getClipData() != null) {
+                        for (int i = 0; i < data.getClipData().getItemCount(); i++) {
+                            files.put(processFileUri(data.getClipData().getItemAt(i).getUri()));
+                        }
+                        this.callback.success(files.toString());
+                    } else if (data.getData() != null) {
+                        files.put(processFileUri(data.getData()));
+                        this.callback.success(files.toString());
+                    } else {
+                        this.callback.error("File URI was null.");
+                    }
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    this.callback.error("RESULT_CANCELED");
+                } else {
+                    this.callback.error(resultCode);
+                }
+            }
+        } catch (Exception err) {
+            this.callback.error("Failed to read file: " + err.toString());
+        }
+    }
+
+    public JSONObject processFileUri(Uri uri) {
+        String fileName = null, extension = null, filePath = null;
+        ContentResolver contentResolver = this.cordova.getActivity().getContentResolver();
+        String name = Chooser.getDisplayName(contentResolver, uri);
+        String mediaType = contentResolver.getType(uri);
+        Boolean lockStatus = false;
+        Cursor cursor = null;
+        if (uri.getScheme().equals("file")) {
+            fileName = uri.getLastPathSegment();
+        } else {
+            try {
+                cursor = cordova.getActivity().getApplicationContext().getContentResolver().query(uri, new String[]{ MediaStore.Images.ImageColumns.DISPLAY_NAME }, null, null, null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    fileName = cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME));
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        filePath = copyFileFromUri(cordova.getActivity().getApplicationContext(), uri, fileName, callback);
+        File file1 = new File(filePath);
+        if (mediaType == null || mediaType.isEmpty()) {
+            mediaType = "application/octet-stream";
+        }
+        JSONObject file = new JSONObject();
+        try {
+            file.put("mediaType", mediaType);
+            file.put("name", name);
+            file.put("filePath", "file://"+file1.getPath());
+            file.put("uri", uri.toString());
+            if(mediaType == "application/pdf"){
+                file.put("lockStatus", isPasswordProtected(file1));   
+            }else{
+                 file.put("lockStatus", false);  
+            }
+        } catch (JSONException err) {
+            this.callback.error("Processing failed: " + err.toString());
+        }
+        return file;
+    }
+    public String copyFileFromUri(Context context, Uri fileUri, String fileName, CallbackContext callback) {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        String outPutFilePath = null;
+        try {
+            ContentResolver content = context.getContentResolver();
+            inputStream = content.openInputStream(fileUri);
+            File saveDirectory = new File(rootDirectory + "/" + "images");
+            saveDirectory.mkdirs();
+            outPutFilePath = saveDirectory + "/" + fileName;
+            outputStream = new FileOutputStream(outPutFilePath);
+            if(outputStream != null) {
+                byte[] buffer = new byte[1000];
+                int bytesRead = 0;
+                while ((bytesRead = inputStream.read( buffer, 0, buffer.length )) >= 0) {
+                    outputStream.write( buffer, 0, buffer.length );
+                }
+            } else {
+                callback.error("Error occurred in opening outputStream");
+            }
+        } catch (Exception e) {
+            callback.error("Exception occurred " + e.getMessage());
+        }
+        return outPutFilePath;
+    }
+     public boolean isPasswordProtected(File pdfFullname) {
+        boolean isValidPdf = false;
+        boolean isLocked = false;
+        try {
+            InputStream tempStream = new FileInputStream(pdfFullname);
+            PdfReader reader = new PdfReader(tempStream);
+            isValidPdf = reader.isOpenedWithFullPermissions();
+            System.out.println(isValidPdf);
+            if(isValidPdf){
+                isLocked = false;
+            }else{
+                isLocked = true;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            isLocked = true;
+        }
+        return isLocked;
+    }
+}
---
+++ cordova-plugin-simple-file-chooser/src/ios/Chooser.swift
@@ -0,0 +1,180 @@
+import UIKit
+import MobileCoreServices
+import Foundation
+import PDFKit
+
+@objc(Chooser)
+class Chooser : CDVPlugin {
+	var commandCallback: String?
+    
+    struct FileInfo: Codable {
+        let mediaType: String
+        let name: String
+        let uri: String
+        let lockStatus: Bool
+     }
+
+	func callPicker (utis: [String]) {
+		let picker = UIDocumentPickerViewController(documentTypes: utis, in: .import)
+		picker.delegate = self
+        if #available(iOS 11.0, *) {
+           // picker.allowsMultipleSelection = true
+        }
+
+		self.viewController.present(picker, animated: true, completion: nil)
+	}
+
+	func detectMimeType (_ url: URL) -> String {
+		if let uti = UTTypeCreatePreferredIdentifierForTag(
+			kUTTagClassFilenameExtension,
+			url.pathExtension as CFString,
+			nil
+		)?.takeRetainedValue() {
+			if let mimetype = UTTypeCopyPreferredTagWithClass(
+				uti,
+				kUTTagClassMIMEType
+			)?.takeRetainedValue() as? String {
+				return mimetype
+			}
+		}
+
+		return "application/octet-stream"
+	}
+
+	func documentWasSelected (urls: [URL]) {
+		var error: NSError?
+        let coordinator = NSFileCoordinator();
+        var results: [FileInfo] = [];
+        for url in urls {
+            coordinator.coordinate(
+                readingItemAt: url,
+                options: [],
+                error: &error
+            ) { newURL in
+                let result = FileInfo(
+                    mediaType: self.detectMimeType(newURL),
+                    name: newURL.lastPathComponent,
+                    uri: newURL.absoluteString,
+                    lockStatus: self.isLocked(newURL.absoluteString)!
+                )
+
+                results.append(result);
+                
+                newURL.stopAccessingSecurityScopedResource()
+            }
+        }
+        do {
+            let jsonData = try JSONEncoder().encode(results)
+            let jsonString = String(data: jsonData, encoding: .utf8)!
+            self.send(jsonString)
+        }
+        catch {
+            self.sendError("Serializing result failed.")
+        }
+        
+		if let error = error {
+			self.sendError(error.localizedDescription)
+		}
+    }
+
+	@objc(getFiles:)
+	func getFiles(command: CDVInvokedUrlCommand) {
+		self.commandCallback = command.callbackId
+
+		let accept = command.arguments.first as! String
+		let mimeTypes = accept.components(separatedBy: ",")
+
+		let utis = mimeTypes.map { (mimeType: String) -> String in
+			switch mimeType {
+				case "audio/*":
+					return kUTTypeAudio as String
+				case "font/*":
+					return "public.font"
+				case "image/*":
+					return kUTTypeImage as String
+				case "text/*":
+					return kUTTypeText as String
+				case "video/*":
+					return kUTTypeVideo as String
+				default:
+					break
+			}
+
+			if mimeType.range(of: "*") == nil {
+				let utiUnmanaged = UTTypeCreatePreferredIdentifierForTag(
+					kUTTagClassMIMEType,
+					mimeType as CFString,
+					nil
+				)
+
+				if let uti = (utiUnmanaged?.takeRetainedValue() as? String) {
+					if !uti.hasPrefix("dyn.") {
+						return uti
+					}
+				}
+			}
+
+			return kUTTypeData as String
+		}
+
+		self.callPicker(utis: utis)
+	}
+
+	func send (_ message: String, _ status: CDVCommandStatus = CDVCommandStatus_OK) {
+		if let callbackId = self.commandCallback {
+			self.commandCallback = nil
+
+			let pluginResult = CDVPluginResult(
+				status: status,
+				messageAs: message
+			)
+
+			self.commandDelegate!.send(
+				pluginResult,
+				callbackId: callbackId
+			)
+		}
+	}
+
+	func sendError (_ message: String) {
+		self.send(message, CDVCommandStatus_ERROR)
+	}
+    
+    public func isLocked(_ filePath: String) -> Bool? {
+        var isProtected = false;
+        let path : NSString = filePath as NSString
+        let thePath = path.expandingTildeInPath
+        let url = URL(string: thePath)!;
+        if let pdfDocument = PDFDocument(url: url) {
+            if pdfDocument.isLocked {
+                print("Yes it's password protected..")
+                isProtected = true;
+            } else {
+                print("No it's not password protected..")
+                isProtected = false;
+            }
+        }
+        return isProtected;
+    }
+}
+
+extension Chooser : UIDocumentPickerDelegate {
+	@available(iOS 11.0, *)
+	func documentPicker (
+		_ controller: UIDocumentPickerViewController,
+		didPickDocumentsAt urls: [URL]
+	) {
+        self.documentWasSelected(urls: urls)
+	}
+
+	func documentPicker (
+		_ controller: UIDocumentPickerViewController,
+		didPickDocumentAt url: URL
+	) {
+		self.documentWasSelected(urls: [url])
+	}
+
+	func documentPickerWasCancelled (_ controller: UIDocumentPickerViewController) {
+		self.sendError("RESULT_CANCELED")
+	}
+}
